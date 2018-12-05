386d79b50a2c606ce1baf7b788b72bb9a52bebfc
AlaaEddin Albarghoth <eng.alaa.brghoth@gmail.com>
2018-12-05 20:59:35 +0300

    [ADD] Adding 'view' package, adding 'ui' package to 'view' package, and move 'MainActivity' class to 'ui' package.

--- app/src/main/java/org/alaaeddin/twittersearchapp/view/ui/MainActivity.java
+++ app/src/main/java/org/alaaeddin/twittersearchapp/view/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package org.alaaeddin.twittersearchapp;
+package org.alaaeddin.twittersearchapp.view.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.alaaeddin.twittersearchapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
