124c9489e696b958fe9c14714be34278f3bdad45
AlaaEddin Albarghoth <eng.alaa.brghoth@gmail.com>
2018-12-06 11:19:40 +0300

    [ADD] 'di' package [ADD] 'component' package to 'di' package [ADD] 'ApplicationComponent' interface to 'component' package

---
+++ app/src/main/java/org/alaaeddin/twittersearchapp/di/component/ApplicationComponent.java
@@ -0,0 +1,10 @@
+package org.alaaeddin.twittersearchapp.di.component;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton // scope annotation
+@Component // tells dagger this interface is a component
+public interface ApplicationComponent {
+}
